79bddd17da717365af909071862290eff96fbb8a
mohammad <mohammadtbz76@gmail.com>
2023-12-23 12:48:44 +0330

    login page added

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -1,20 +1,19 @@
 <x-auth-layout>
 
     <!--begin::Form-->
-    <form class="form w-100" novalidate="novalidate" id="kt_sign_in_form" data-kt-redirect-url="/" action="login">
+    <form class="form w-100" method="post" action="{{ route("login") }}">
         @csrf
+        <input type="hidden" name="guard" value="admin">
         <!--begin::Heading-->
         <div class="text-center mb-11">
             <!--begin::Title-->
             <h1 class="text-dark fw-bolder mb-3">
-                Sign In
+                ورود به پنل مدیریتی
             </h1>
             <!--end::Title-->
 
             <!--begin::Subtitle-->
-            <div class="text-gray-500 fw-semibold fs-6">
-                Your Social Campaigns
-            </div>
+
             <!--end::Subtitle--->
         </div>
         <!--begin::Heading-->
@@ -24,10 +23,10 @@
             <!--begin::Col-->
             <div class="col-md-6">
                 <!--begin::Google link--->
-                <a href="{{ url('/auth/redirect/google') }}?redirect_uri={{ url()->current() }}" class="btn btn-flex btn-outline btn-text-gray-700 btn-active-color-primary bg-state-light flex-center text-nowrap w-100">
+                {{--<a href="{{ url('/auth/redirect/google') }}?redirect_uri={{ url()->current() }}" class="btn btn-flex btn-outline btn-text-gray-700 btn-active-color-primary bg-state-light flex-center text-nowrap w-100">
                     <img alt="Logo" src="{{ image('svg/brand-logos/google-icon.svg') }}" class="h-15px me-3"/>
                     Sign in with Google
-                </a>
+                </a>--}}
                 <!--end::Google link--->
             </div>
             <!--end::Col-->
@@ -35,22 +34,17 @@
             <!--begin::Col-->
             <div class="col-md-6">
                 <!--begin::Google link--->
-                <a href="{{ url('/auth/redirect/apple') }}?redirect_uri={{ url()->current() }}" class="btn btn-flex btn-outline btn-text-gray-700 btn-active-color-primary bg-state-light flex-center text-nowrap w-100">
+             {{--   <a href="{{ url('/auth/redirect/apple') }}?redirect_uri={{ url()->current() }}" class="btn btn-flex btn-outline btn-text-gray-700 btn-active-color-primary bg-state-light flex-center text-nowrap w-100">
                     <img alt="Logo" src="{{ image('svg/brand-logos/apple-black.svg') }}" class="theme-light-show h-15px me-3"/>
                     <img alt="Logo" src="{{ image('svg/brand-logos/apple-black-dark.svg') }}" class="theme-dark-show h-15px me-3"/>
                     Sign in with Apple
-                </a>
+                </a>--}}
                 <!--end::Google link--->
             </div>
             <!--end::Col-->
         </div>
         <!--end::Login options-->
 
-        <!--begin::Separator-->
-        <div class="separator separator-content my-14">
-            <span class="w-125px text-gray-500 fw-semibold fs-7">Or with email</span>
-        </div>
-        <!--end::Separator-->
 
         <!--begin::Input group--->
         <div class="fv-row mb-8">
@@ -72,30 +66,21 @@
             <div></div>
 
             <!--begin::Link-->
-            <a href="/forgot-password" class="link-primary">
+         {{--   <a href="/forgot-password" class="link-primary">
                 Forgot Password ?
-            </a>
+            </a>--}}
             <!--end::Link-->
         </div>
         <!--end::Wrapper-->
 
         <!--begin::Submit button-->
         <div class="d-grid mb-10">
-            <button type="submit" id="kt_sign_in_submit" class="btn btn-primary">
-                @include('partials/general/_button-indicator', ['label' => 'Sign In'])
+            <button type="submit" class="btn btn-primary">
+                ورود
             </button>
         </div>
         <!--end::Submit button-->
 
-        <!--begin::Sign up-->
-        <div class="text-gray-500 text-center fw-semibold fs-6">
-            Not a Member yet?
-
-            <a href="/register" class="link-primary">
-                Sign up
-            </a>
-        </div>
-        <!--end::Sign up-->
     </form>
     <!--end::Form-->
 
